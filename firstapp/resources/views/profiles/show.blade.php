dd65d76e6bbd16359718e7d3f96a7743617eb6fa
MilleCodesLN <mille.helene@outlook.fr>
2020-07-30 14:41:43 +0200

    affichage barre auth

---
+++ firstapp/resources/views/profiles/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-4">
+          <img src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/e35/s150x150/100091395_613567476177619_1184006016704636123_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=107&_nc_ohc=9SM7RPOS0P0AX99Y--k&oh=9667a741f3a7d27fa66b67da64b27b63&oe=5F4E1EF2" alt="" class="rounded-circle">
+      </div>
+      <div class="col-8">
+        <div class="d-flex">
+
+            <div class=' h4 mr-3 pt-2'>LaraGram</div>
+            <button class='btn btn-primary'>S'abonner</button>
+        </div>
+      </div>
+
+    </div>
+</div>
+@endsection
